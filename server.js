c67a54eaafbe43b384f77c11e2669ed014e4be62
Adetayo <akinsanyaadetayo7@gmail.com>
2020-07-17 12:46:48 +0100

    initial commit server

--- server.js
+++ server.js
@@ -6,6 +6,36 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'show all bootcamps' });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `show bootcamps ${req.params.id}` });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create new bootcamps' });
+});
+
+app.post('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamps ${req.params.id}` });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamps ${req.params.id}` });
+});
+
+app.delete('/api/v1/bootcamp', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Delete  new bootcamp' });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
