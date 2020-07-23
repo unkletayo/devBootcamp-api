5f3f9317729a77340f673edda9b68ee5b3d56dd6
Adetayo <akinsanyaadetayo7@gmail.com>
2020-07-23 19:56:30 +0100

    Chore: Add schema for courses
    Add Schema for courses and Implemented in seeder file

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/config.env' });
 
 // load models
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 // connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -21,11 +22,15 @@ mongoose.connect(process.env.MONGO_URI, {
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
 
 // Import data to Database
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
 
     console.log('Data Imported ...'.green.inverse);
     process.exit();
@@ -38,6 +43,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
 
     console.log('Data Destroyed ...'.red.inverse);
     process.exit();
