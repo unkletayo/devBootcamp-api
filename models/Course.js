ae9fb15aa0d68505c366189696bd78ed0bad18b3
Adetayo <akinsanyaadetayo7@gmail.com>
2020-07-24 11:36:20 +0100

    Chore: Implement update and delete course API

--- models/Course.js
+++ models/Course.js
@@ -38,4 +38,36 @@ const CourseSchema = new mongoose.Schema({
   },
 });
 
+// static method to get average of cost tuition
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageCost: { $avg: '$tuition' },
+      },
+    },
+  ]);
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageCost: Math.ceil(obj[0].averageCost / 10) * 10,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// call getAverage middleware after save
+CourseSchema.post('save', function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
+// call getAverage middleware before delete
+CourseSchema.post('pre', function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
 module.exports = mongoose.model('Course', CourseSchema);
