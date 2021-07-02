676654275b3c4693e8c067afbff5ec5685636584
Adetayo <akinsanyaadetayo7@gmail.com>
2021-07-02 22:10:11 +0100

    Admin CRUD

--- controllers/auth.js
+++ controllers/auth.js
@@ -58,6 +58,25 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+
+/*
+@Des   Log user out /clear cookie
+@route GET /api/v1/auth/logout
+@access Private
+*/
+
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true
+  })
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
+
 /*
 @Des  Get current login user
 @route POST /api/v1/auth/me
--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,25 @@
-const express = require('express')
-const  {register, login, getMe, forgotPassword, resetPassword, updateDetails, updatePassword} = require('../controlLers/auth');
+const express = require('express');
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword,
+  resetPassword,
+  updateDetails,
+  updatePassword,
+  logout,
+} = require('../controlLers/auth');
 const { protect } = require('../middleware/auth');
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/register', register)
-router.post('/login', login)
-router.get('/me', protect,  getMe)
-router.put('/update-details', protect,  updateDetails)
-router.put('/update-password', protect,  updatePassword)
-router.post('/forgot-password', forgotPassword)
-router.put('/reset-password/:resettoken', resetPassword)
+router.post('/register', register);
+router.post('/login', login);
+router.post('/logout', protect, logout);
+router.get('/me', protect, getMe);
+router.put('/update-details', protect, updateDetails);
+router.put('/update-password', protect, updatePassword);
+router.post('/forgot-password', forgotPassword);
+router.put('/reset-password/:resettoken', resetPassword);
 
-module.exports = router
+module.exports = router;
